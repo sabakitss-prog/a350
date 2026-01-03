3e06e348216dbf712a400e500dc44c966c143ac4
sabakitss-prog <sabakitss@gmail.com>
2026-01-03 12:03:09 +0300

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,14 @@
+let score = 0;
+
+function checkAnswer(button, correct) {
+  if (button.classList.contains("correct") || button.classList.contains("wrong")) return;
+
+  if (correct) {
+    button.classList.add("correct");
+    score++;
+  } else {
+    button.classList.add("wrong");
+  }
+
+  document.getElementById("score").innerText = "Score: " + score;
+}
